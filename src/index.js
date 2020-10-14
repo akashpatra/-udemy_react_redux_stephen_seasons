723361dbe511b26eb361aeec059a7ffa9682904f
Akash Patra <patraakash9@gmail.com>
2020-10-14 16:42:01 +0530

    feat: Seasons App using Class, State and Lifecycle Methods

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered');
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please accept Location Request" />;
+    }
+
+    // React says we have to define render!!
+    render() {
+        console.log('render called')
+
+        return <div className="border red">{this.renderContent()}</div>
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
